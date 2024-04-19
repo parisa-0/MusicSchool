34b1924d9421701221b33f6812d3c9d69e708b5c
I337592 <parisanoushazaran@gmail.com>
2024-04-19 14:50:39 +0100

    blueprint for services classes and what's needed there

---
+++ src/main/java/Service/CourseService.java
@@ -0,0 +1,22 @@
+package Service;
+
+import Model.Courses;
+
+public class CourseService {
+
+    public void addCourse(Courses course) {
+
+    }
+
+  //  public courses getCourse(String courseID) {
+//
+  //  }
+
+ //book a course
+
+   //find course
+
+   //print all available courses
+
+   //getcoursePerInstructor
+}
---
+++ src/main/java/Service/InstructorService.java
@@ -0,0 +1,20 @@
+package Service;
+
+import Model.Instructor;
+
+import java.util.Collection;
+
+public class InstructorService {
+
+    public void addInstructor(String username, String password, String firstName, String lastName, String instructorId) {
+
+    }
+
+    public void getInstructor(String instructorId) {
+
+    }
+
+  //  public Collection<Instructor> getAllInstructors() {
+    // }
+
+}
